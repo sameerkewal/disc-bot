d98a6af0481d746ccc2fee53810ff856354e9eb3
skewal <sameerkewal1@gmail.com>
2025-05-25 23:12:58 -0300

    changes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const server = express();
 
+const port = process.env.port || 4000;
+console.log("binding on port ", port);
+
 server.all(`/`, (req, res) => {
     res.send(`Result: [OK].`);
 });
 
 function keepAlive() {
-    server.listen(3000, () => {
+    server.listen(port, () => {
         console.log(`Server is now ready! | ` + Date.now());
     });
 }
