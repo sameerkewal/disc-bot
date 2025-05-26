95c9ee5094ab7d68c615355a0639065f8c82a2e5
skewal <sameerkewal1@gmail.com>
2025-05-25 23:29:48 -0300

    changes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 console.log("binding on port ", port);
 
 server.all(`/`, (req, res) => {
