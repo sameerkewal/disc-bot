81c658a058c95740cedad7707bfd4ecd7efe8616
skewal <sameerkewal1@gmail.com>
2025-05-31 16:47:21 -0300

    changes

--- src/server.js
+++ src/server.js
@@ -16,26 +16,27 @@ server.get("/callback", (req, res) => {
     const code = req.query.code;
     const state = req.query.state;
 
-    requestAccessToken(state, code).then(() => {
-        res.send(`
-      <html lang="en">
-        <body>
-          <h3>Authentication complete! You can now return to the Discord app or browser.</h3>
-          <p><a href="https://discord.com/channels/@me">Go to Discord</a></p>
-        </body>
-      </html>
-    `)
-            .catch((error) => {
-                console.error("Error during auth:", error);
-                res.send(`
-      <html lang="en">
-        <body>
-          <h3>Unauthorized!</h3>
-        </body>
-      </html>
-    `);
-            });
-    })
+    requestAccessToken(state, code)
+        .then(() => {
+            res.send(`
+          <html lang="en">
+            <body>
+              <h3>Authentication complete! You can now return to the Discord app or browser.</h3>
+              <p><a href="https://discord.com/channels/@me">Go to Discord</a></p>
+            </body>
+          </html>
+        `);
+        })
+        .catch((error) => {
+            console.error("Error during authentication:", error);
+            res.status(401).send(`
+          <html lang="en">
+            <body>
+              <h3>Unauthorized!</h3>
+            </body>
+          </html>
+        `);
+        });
 })
 
 
